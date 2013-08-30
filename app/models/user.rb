344f30e10be2eabf583d8bb7ec130e0e3a11d3ce
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 14:45:39 -0500

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  validates :name, :email, :password, presence: true 
   has_many :rounds
 
   def self.validate(email, password)
