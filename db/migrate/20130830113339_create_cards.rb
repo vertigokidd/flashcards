a4b8ea90fc5387c6ea866b1c8c1fe39968f19637
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 14:07:11 -0500

    re-seed

--- db/migrate/20130830113339_create_cards.rb
+++ db/migrate/20130830113339_create_cards.rb
@@ -1,6 +1,7 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
+      t.belongs_to :deck
       t.string :question
       t.string :answer
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 require 'CSV'
 
-Deck.create!(:name => "computer")
+deck = Deck.create!(:name => "computer")
+
+# deck.cards = [array of all card objects]
+# deck.cards << shovel things in here
 
 CSV.foreach('cards.csv') do |row|
-  Card.create!(:question => row[1], :answer => row[0])
+  deck.cards << Card.create!(:question => row[1], :answer => row[0])
 end     
