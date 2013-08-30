50e1c05ee61a09ef0e09b3d371eb0305ea053c15
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-08-30 13:11:13 -0500

    Page layouts

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,37 @@
+# GET ========================================
+
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+
+get '/account/:id' do
+  #if session cookie == :id
+   # go to taht user's page
+   #else
+   #redirect to index
+end
+
+
+get '/card/:card_id' do
+  erb :card
+end
+
+
+
+
+# POST =======================================
+
+
+post '/account' do
+
+  redirect "/account/#{user.id}"
+end
+
+post '/create' do
+  # Validate and create a new user
+  # Set a session value to user's id
+  redirect "/account/#{user.id}"
 end
